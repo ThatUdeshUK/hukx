d2c9ba69358d0a802400b9c273d66a0262aa41e3
Udesh Kumarasinghe <mail@udeshuk.me>
2019-05-04 08:12:35 +0530

    Resolved dependencies

--- sample/app.js
+++ sample/app.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -11,8 +9,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
